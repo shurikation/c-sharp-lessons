b182ba174b235fac25e68a71fe703df05da67025
shurikation <shurikation@gmail.com>
2021-06-01 16:40:20 +0400

    finished lesson#4: variables

--- c-sharp/Program.cs
+++ c-sharp/Program.cs
@@ -1,32 +1,29 @@
 ﻿using System;
 
-namespace DataTypes
+namespace Lessons
 {
 	class Program
 	{	
-		//Точка входа в программу
 		static void Main(string[] args)
 		{
+			int b = 5;
+			b = 100;
 
+			bool variable = false;
+			bool Variable = true;
+
+			double someNumber = 1.44;
+
+			char a = '✛';
+
+			
+			Console.WriteLine(b);
+	
+		
 		}
 	}
 }
 
 /*
- * Управляемая куча при выделении памяти
- * Управляется CLR - которая следит за утечкой памяти
- * При компиляции она копилисриуется на CIL с помощью...
- * ...JIT - Just In Time компиляция - пробразовывает асемблерный код в машинный
- *
- *using - подключение пространств имен
- *это блоки кода которые разграничивают классы, код чтобы они не конфликовали
- *белые юзинги испольуются, серые - нет
- *Так, класс Console подключается из пространства имени System
- *Если не подключено System, то придется писать System.Console.WriteLine("Hello world!");
- */
 
-/*
- *  Типы данных
- *  Object - особый тип и все другие типы наследуются от него
- *  Dynamic
- */
+*/
