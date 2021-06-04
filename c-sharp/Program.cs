a26c26b9e6bc778377aec55d17e8f1238f5abb8e
shurikation <shurikation@gmail.com>
2021-06-04 16:49:56 +0400

    finished lesson #6: convert string

--- c-sharp/Program.cs
+++ c-sharp/Program.cs
@@ -1,20 +1,49 @@
 ﻿using System;
+using System.Globalization; //Просторанство имён
+/**
+ * #6 - Конвертация строки в число
+ */
 
 namespace Lessons
 {
 	class Program
-	{	
+	{
 		static void Main(string[] args)
 		{
-			string data;
-			char smile = ')';
-			data = Console.ReadLine();
+			/*
+			string str;
+			int a, b;
+			Console.WriteLine("Enter number 1");
+			str = Console.ReadLine();
+			a = Convert.ToInt32(str);
+			Console.WriteLine("Enter number 2");
+			str = Console.ReadLine();
+			b = Convert.ToInt32(str);
+			int result = a + b;
+			Console.WriteLine("Сумма чисел = " + result);
+			*/
 
-			Console.WriteLine("Привет, " + data + " :)");
+			string str2 = "1.9";
+		
+
+			//Подключаем выше using System.Globalization;
+			//Создаем экземпляр класса NumberFormatInfo
+			NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+			{
+				NumberDecimalSeparator = ".", //указываем разделитель
+			};
+
+			double a = Convert.ToDouble(str2, numberFormatInfo);
+			Console.WriteLine(a);
 		}
 	}
 }
 
 /*
- В С# поддерживается юникод - нет проблем с анг-русским языком
-*/
+ * Конвертация строки в число происходит с помощью Convert.
+ * Но мето не всегдла может конвертнуть в double строку с точкой: 1.9
+ * Ему нужна 1,9 - с запятой
+ * Могут быть разыне настрйоки в заивсимости от региона, метод не всегда работает
+ * 
+ * */
+
