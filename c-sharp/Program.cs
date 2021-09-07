60fe7db580b7dfde0be33d6cac585ce617658682
shurikation <shurikation@gmail.com>
2021-09-07 16:27:48 +0400

    finished lesson #7: parsing, try catch, tryParse

--- c-sharp/Program.cs
+++ c-sharp/Program.cs
@@ -9,35 +9,61 @@ namespace Lessons
 	class Program
 	{
 		static void Main(string[] args)
-		{
-			/*
-			string str;
-			int a, b;
-			Console.WriteLine("Enter number 1");
-			str = Console.ReadLine();
-			a = Convert.ToInt32(str);
-			Console.WriteLine("Enter number 2");
-			str = Console.ReadLine();
-			b = Convert.ToInt32(str);
-			int result = a + b;
-			Console.WriteLine("Сумма чисел = " + result);
-			*/
+		{	
+			//Еще один способ конвертации
+			//Parse заточен именно на работу со строками, в отличие от общего Convert
+			string str = "5";
+			int a = int.Parse(str);
 
-			string str2 = "1.9";
-		
+			//Для числа с плавающей точкой
+			string str2 = "5.9";
+			double a = double.Parse(str2);
 
-			//Подключаем выше using System.Globalization;
-			//Создаем экземпляр класса NumberFormatInfo
+			//Может быть как с точкой, так и с запятой в заивисмоти о региональных настроек
 			NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
 			{
-				NumberDecimalSeparator = ".", //указываем разделитель
+				NumberDecimalSeparator = " . ",
 			};
+			double b = double.Parse(str2, numberFormatInfo);
 
-			double a = Convert.ToDouble(str2, numberFormatInfo);
-			Console.WriteLine(a);
+			//Если в строку приходит какая-то фигня
+			string str3 = "5.9выыфвыфв";
+			double с = double.Parse(str3); //получим ошибку
+			int d = Convert.ToInt32(str3); //...и тут получаем ошибку
+
+			//Можно использовать try catch
+			try
+			{
+				int f = Convert.ToInt32(str3);
+			}
+			catch(Exception)
+			{
+				Console.WriteLine("Its fuckin convertation error!");
+			}
+
+			//Или tryparse - который не бросает исключения в случае ошибки в отличие от parse or convert
+			int a2;
+
+			int.TryParse(str, out a2); //out - перемеенная будет инициализирована внутри метода и возвращена
+
+			//Если он не сможет распарсить, то в a2 уйдет 0 - значение по умолчанию
+			//Поэтому лучше вернуть булевый результат
+
+			bool result = int.TryParse(str, out a2);
+			if(result)
+			{
+				Console.WriteLine("Succeces!");
+			}
+			else
+			{
+				Console.WriteLine("Error!");
+			}
+
 		}
 	}
 }
+
+
 
 /*
  * Конвертация строки в число происходит с помощью Convert.
