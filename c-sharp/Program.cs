406c854745e5a072fe8919be197ad3e7200cae73
shurikation <shurikation@gmail.com>
2021-09-07 16:59:36 +0400

    finished lesson #8: operators

--- c-sharp/Program.cs
+++ c-sharp/Program.cs
@@ -3,73 +3,34 @@ using System.Globalization; //Просторанство имён
 /**
  * #6 - Конвертация строки в число
  */
-
 namespace Lessons
 {
 	class Program
 	{
 		static void Main(string[] args)
-		{	
-			//Еще один способ конвертации
-			//Parse заточен именно на работу со строками, в отличие от общего Convert
-			string str = "5";
-			int a = int.Parse(str);
+		{
+			double a = 2; //нужно одну перемнную привести к дабл
+			int b = 5;
 
-			//Для числа с плавающей точкой
-			string str2 = "5.9";
-			double a2 = double.Parse(str2);
+			double result = (double) a / b; //int не хранит дробную часть
+			//мы выполняем приведение к типу (double)
+			Console.WriteLine(result);
 
-			//Может быть как с точкой, так и с запятой в заивисмоти о региональных настроек
-			NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
-			{
-				NumberDecimalSeparator = " . ",
-			};
-			double b = double.Parse(str2, numberFormatInfo);
+			int a1 = 10;
+			int b1 = 3;
 
-			//Если в строку приходит какая-то фигня
-			string str3 = "5.9выыфвыфв";
-			double с = double.Parse(str3); //получим ошибку
-			int d = Convert.ToInt32(str3); //...и тут получаем ошибку
+			//int или просто число это int32, даже если число 5 не присовено в переменную
+			int c = a + 5;
 
-			//Можно использовать try catch
-			try
-			{
-				int f = Convert.ToInt32(str3);
-			}
-			catch(Exception)
-			{
-				Console.WriteLine("Its fuckin convertation error!");
-			}
-
-			//Или tryparse - который не бросает исключения в случае ошибки в отличие от parse or convert
-			int a3;
-
-			int.TryParse(str, out a3); //out - перемеенная будет инициализирована внутри метода и возвращена
-
-			//Если он не сможет распарсить, то в a2 уйдет 0 - значение по умолчанию
-			//Поэтому лучше вернуть булевый результат
-
-			bool result = int.TryParse(str, out a3);
-			if(result)
-			{
-				Console.WriteLine("Succeces!");
-			}
-			else
-			{
-				Console.WriteLine("Error!");
-			}
+			int result1 = a1 % b1; //остаток от деления
 
 		}
 	}
+
 }
 
 
 
-/*
- * Конвертация строки в число происходит с помощью Convert.
- * Но мето не всегдла может конвертнуть в double строку с точкой: 1.9
- * Ему нужна 1,9 - с запятой
- * Могут быть разыне настрйоки в заивсимости от региона, метод не всегда работает
- * 
- * */
+
+
 
